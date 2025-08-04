1841bc3fdaa73236f3ac9e9465d63f4158b62f0e
Mariana Milani <mari.m.assuncao@gmail.com>
2025-08-04 15:53:18 -0300

    first commit

---
+++ src/main/java/br/com/datamovie/DatamovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.datamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/datamovie/DatamovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.datamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
