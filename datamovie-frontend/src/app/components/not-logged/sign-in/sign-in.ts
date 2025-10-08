ce9d0c8058cb96de277179502a9020e613b268ce
Mariana Milani <mari.m.assuncao@gmail.com>
2025-10-08 15:51:33 -0300

    feat: Includes login validation

--- datamovie-frontend/src/app/components/not-logged/sign-in/sign-in.ts
+++ datamovie-frontend/src/app/components/not-logged/sign-in/sign-in.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {RouterLink, Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
+import {User} from '../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css'
 })
 export class SignIn {
+  user: User = {
+    email: '',
+    password: '',
+  };
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    if(!this.user.email || !this.user.password || this.user.password.length < 1) {
+      return;
+    }
+
+
+    this.authService.login(this.user).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.log('Erro no login: ', error);
+      }
+    });
+  }
 }
---
+++ datamovie-frontend/src/app/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  email: string;
+  password: string;
+}
+
+export interface UserRegister extends User {
+  name: string;
+}
+
+export interface UserResponse {
+  token: string;
+}
